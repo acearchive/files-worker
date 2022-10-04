6021eb60709b9aa0d4efd6ffa2c88058a39502fe
Frawley <frawleyskid@gmail.com>
2022-10-04 14:35:56 -0400

    Refactor URL parsing an error handling logic

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,45 @@ interface Env {
   ARTIFACTS_R2: R2Bucket;
 }
 
-const notFound = (request: Request): Response =>
-  new Response(`File ${new URL(request.url).pathname} not found.`, {
-    status: 404,
-  });
+const Status = {
+  NotFound(request: Request): Response {
+    return new Response(`File ${request.url} not found.`, {
+      status: 404,
+    });
+  },
 
-const methodNotAllowed = (request: Request): Response =>
-  new Response(`Method ${request.method} not allowed.`, {
-    status: 405,
-    headers: { Allow: "GET, HEAD" },
-  });
+  MethodNotAllowed(request: Request): Response {
+    return new Response(`Method ${request.method} not allowed.`, {
+      status: 405,
+      headers: { Allow: "GET, HEAD" },
+    });
+  },
+};
+
+type ParseUrlResult =
+  | { isValid: true; artifactSlug: string; fileName: string }
+  | { isValid: false; response: Response };
+
+const parseUrl = (request: Request): ParseUrlResult => {
+  const url = new URL(request.url);
+  const pathComponents = url.pathname.split("/", 3);
+
+  if (pathComponents.length < 3) {
+    return { isValid: false, response: Status.NotFound(request) };
+  }
+
+  const [namespace, artifactSlug, fileName] = pathComponents;
+
+  if (
+    namespace !== "artifacts" ||
+    artifactSlug.length === 0 ||
+    fileName.length === 0
+  ) {
+    return { isValid: false, response: Status.NotFound(request) };
+  }
+
+  return { isValid: true, artifactSlug, fileName };
+};
 
 export default {
   async fetch(
@@ -21,24 +50,18 @@ export default {
     ctx: ExecutionContext
   ): Promise<Response> {
     if (request.method !== "GET" && request.method !== "HEAD") {
-      return methodNotAllowed(request);
+      return Status.MethodNotAllowed(request);
     }
 
-    const url = new URL(request.url);
-    const pathComponents = url.pathname.split("/");
-
-    if (pathComponents.length !== 3) {
-      return notFound(request);
+    const urlResult = parseUrl(request);
+    if (!urlResult.isValid) {
+      return urlResult.response;
     }
 
-    const [namespace, artifactSlug, fileName] = pathComponents;
+    const { artifactSlug, fileName } = urlResult;
 
-    if (
-      namespace !== "artifacts" ||
-      artifactSlug.length === 0 ||
-      fileName.length === 0
-    ) {
-      return notFound(request);
-    }
+    return new Response(JSON.stringify({ artifactSlug, fileName }), {
+      status: 200,
+    });
   },
 };
