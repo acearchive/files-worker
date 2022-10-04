cda85da6b9589fa73efb4ef13db2f0b898c7302b
Frawley <frawleyskid@gmail.com>
2022-10-04 01:42:54 -0400

    Add types for KV and R2 bindings

--- functions/artifacts/[[path]].ts
+++ functions/artifacts/[[path]].ts
@@ -1,6 +1,28 @@
-export const onRequestGet: PagesFunction<{ request: Request }> = async ({
-  request,
-}) => {
+interface Env {
+  ARTIFACTS_KV: KVNamespace;
+  ARTIFACTS_R2: R2Bucket;
+}
+
+const notFound = (): Response => new Response(undefined, { status: 404 });
+
+export const onRequestGet: PagesFunction<{
+  request: Request;
+  env: Env;
+}> = async ({ request, env }) => {
+  if (request.method !== "GET" && request.method !== "HEAD") {
+    return new Response(undefined, { status: 405 });
+  }
+
   const url = new URL(request.url);
-  const [artifactSlug, fileName] = url.pathname.split("/", 2);
+  const pathComponents = url.pathname.split("/");
+
+  if (pathComponents.length !== 2) {
+    return notFound();
+  }
+
+  const [artifactSlug, fileName] = pathComponents;
+
+  if (artifactSlug.length === 0 || fileName.length === 0) {
+    return notFound();
+  }
 };
