bda083b188e54c35fb4824f6a3e579b273260fc9
Frawley <frawley@duck.com>
2025-07-16 07:19:31 -0400

    Show toast on clipboard copy

--- src/html.ts
+++ src/html.ts
@@ -38,35 +38,47 @@ const baseFilePageTemplate = ({
         ${unsafeEmbed}
       </main>
       <footer>
-        <a href="${encodeURI(artifactPageUrl)}">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
-            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-            <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
-          </svg>
-          <span>Info<span>
-        </a>
-        <a href="${encodeURI(rawFileUrl)}">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark" viewBox="0 0 16 16">
-            <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5z"/>
-          </svg>
-          <span>Raw</span>
-        </a>
-        <button type="button" id="copy-short-url-button" data-url="${encodeURI(
+        <div id="toast" hidden>
+          <span class="toast-message" id="copy-toast">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-clipboard-check" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0"/>
+              <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z"/>
+              <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z"/>
+            </svg>
+            URL copied to clipboard!
+          </span>
+        </div>
+        <div id="buttons">
+          <a href="${encodeURI(artifactPageUrl)}">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+              <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
+            </svg>
+            <span>Info<span>
+          </a>
+          <a href="${encodeURI(rawFileUrl)}">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark" viewBox="0 0 16 16">
+              <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5z"/>
+            </svg>
+            <span>Raw</span>
+          </a>
+          <button type="button" id="copy-short-url-button" data-url="${encodeURI(
   shortFileUrl
 )}">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share-fill" viewBox="0 0 16 16">
-            <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.5 2.5 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5"/>
-          </svg>
-          <span>Short URL</span>
-        </button>
-        <button type="button" id="copy-short-raw-url-button" data-url="${encodeURI(
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share-fill" viewBox="0 0 16 16">
+              <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.5 2.5 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5"/>
+            </svg>
+            <span>Short URL</span>
+          </button>
+          <button type="button" id="copy-short-raw-url-button" data-url="${encodeURI(
   shortRawFileUrl
 )}">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share-fill" viewBox="0 0 16 16">
-            <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.5 2.5 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5"/>
-          </svg>
-          <span>Short Raw URL</span>
-        </button>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share-fill" viewBox="0 0 16 16">
+              <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.5 2.5 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5"/>
+            </svg>
+            <span>Short Raw URL</span>
+          </button>
+        </div>
       </footer>
     </body>
   </html>
@@ -306,28 +318,58 @@ export const filePageStyles = `
     bottom: 1rem;
     display: flex;
     gap: 1rem;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  #buttons, #toast {
+    display: flex;
+    gap: 1rem;
     border-radius: 1rem;
     background-color: #212121;
     padding: 0.8rem;
+    margin-left: auto;
+    margin-right: auto;
     filter: drop-shadow(2px 2px 2px black);
+  }
+
+  #toast {
+    color: #dee2e6;
+  }
+
+  #toast[hidden] {
+    display: none;
+  }
+
+  .toast-message {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
   }
 `;
 
 export const filePageScript = `
   const copyShortFileUrlButton = document.getElementById("copy-short-url-button");
   const copyShortRawFileUrlButton = document.getElementById("copy-short-raw-url-button");
+  const toast = document.getElementById("toast");
 
   copyShortFileUrlButton.addEventListener("click", () => {
     const url = copyShortFileUrlButton.dataset.url;
     navigator.clipboard.writeText(url).then(() => {
-      alert("Short file URL copied to clipboard!");
+      toast.hidden = false;
+      setTimeout(() => {
+        toast.hidden = true;
+      }, 1500)
     });
   });
 
   copyShortRawFileUrlButton.addEventListener("click", () => {
     const url = copyShortRawFileUrlButton.dataset.url;
     navigator.clipboard.writeText(url).then(() => {
-      alert("Short raw file URL copied to clipboard!");
+      toast.hidden = false;
+      setTimeout(() => {
+        toast.hidden = true;
+      }, 1500)
     });
   });
 `;
